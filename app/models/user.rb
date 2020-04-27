3675845ff99c0580ba20888133645be3cfd79570
kip <cyruskiprop254@gmail.com>
2020-04-27 17:04:38 +0300

    add association between user(event-creator) and the events models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_181940) do
+ActiveRecord::Schema.define(version: 2020_04_26_191025) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_04_26_181940) do
     t.datetime "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
