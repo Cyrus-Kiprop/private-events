2acec2d5fd66edb41c32d450bd257e6f99f27031
kip <cyruskiprop254@gmail.com>
2020-04-28 15:50:45 +0300

    add a many to many assoc between events and users

--- db/migrate/20200427181100_add_column_to_tickets.rb
+++ db/migrate/20200427181100_add_column_to_tickets.rb
@@ -1,6 +1,6 @@
 class AddColumnToTickets < ActiveRecord::Migration[5.2]
   def change
-    add_reference :tickets, :attendee, foreign_key: true
-    add_reference :tickets, :event_attended, foreign_key: true
+    add_reference :tickets, :user, foreign_key: true
+    add_reference :tickets, :event, foreign_key: true
   end
 end
